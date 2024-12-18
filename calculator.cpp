f99f6a16d5a66c3e3f59adb7bf73b384cc3a4c8d
Azot5 <wart4158@gmail.com>
2024-12-18 16:00:13 +0200

    Update calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -1,14 +1,5 @@
 #include "calculator.h"
 
-int Calculator::Add (double a, double b)
-{
-<<<<<<< HEAD
-	return a + b;
-=======
-	return a + b + 0.5;
->>>>>>> 4ad40a3b4572deee4efc716e7bd6e6e4fda46dcf
-}
-
 int Calculator::Sub (double a, double b)
 {
 	return Add (a, -b);
@@ -19,7 +10,6 @@ int Calculator::Mul (double a, double b)
     return a * b + 0.5;
 }
 
-// Додавання нової операції: обчислення залишку від ділення
 int modulus(int a, int b) {
   if (b == 0) {
     throw std::invalid_argument("Division by zero");
